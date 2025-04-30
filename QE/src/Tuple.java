14da6fe85054d1452a75aab256bd375a71976fef
AnkleG <akhilguttikonda1@gmail.com>
2025-04-30 13:44:21 -0400

    Update Tuple.java

--- QE/src/Tuple.java
+++ QE/src/Tuple.java
@@ -13,23 +13,48 @@ public class Tuple implements ITuple {
     /**
      * The constructor receives a schema and creates the object array and typemap (representing the tuple)
      * The schema has the types of attributes stored as strings ("Integer", "Double", "String")
-     * Based upon these types the constructor stores the actual class (Integer.class, Double.class, String.class) to the typemap
+     * Based upon these types the constructor stores the actual class (Integer.class, Double.class,
+     * String.class) to the typemap
      * @param schema
      */
     public Tuple(ISchema schema) {
-
+        this.typeMap = new HashMap<>();
+        for (int i = 0; i < schema.getAttributes().size();i++){
+            Class<?> c;
+            if (schema.getType(i) == "String"){
+                c = String.class;
+            }
+            else if (schema.getType(i) == "Integer") {
+                c = Integer.class;
+            }
+            else {
+                c = Double.class;
+            }
+            this.typeMap.put(i,c);
+        }
+        this.values = new Object[this.typeMap.size()];
     }
 
     /**
      * Stores the value at the given index in the (tuple) object
-     * The value is converted from the object to to its actual class from the typemap
+     * The value is converted from the object to its actual class from the typemap
      * @param index
      * @param value
      */
     @Override
 
     public void setValue(int index, Object value) {
-
+        Class<?> c = typeMap.get(index);
+        if (c == Integer.class){
+            value = Integer.parseInt(c.toString());
+        }
+        else if (c == Double.class) {
+            value = Double.parseDouble(c.toString());
+        }
+        else if (!c.isInstance(value)){
+            throw new IllegalArgumentException("Not a valid CLass");
+        }
+        values[index] = value;
     }
 
     /**
@@ -39,9 +64,8 @@ public class Tuple implements ITuple {
      * @param <T>
      */
     @Override
-    @SuppressWarnings("unchecked")
     public <T> T getValue(int index) {
-
+        return (T) this.values[index];
     }
 
     /**
@@ -50,7 +74,7 @@ public class Tuple implements ITuple {
      */
     @Override
     public Object[] getValues() {
-
+        return this.values;
     }
 
     /**
@@ -60,6 +84,6 @@ public class Tuple implements ITuple {
      */
     @Override
     public void setValues(Object[] values) {
-
+        this.values = values;
     }
 }
